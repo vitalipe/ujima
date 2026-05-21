c7a2283ee6e2b9ed633c16cd3b6a7245b08d3fe4
vitalipe <vitali.pe@gmail.com>
2026-05-21 18:15:21 +0300

    refactor rpi-runtime

--- ujima-os/src/ujima/target.clj
+++ ujima-os/src/ujima/target.clj
@@ -1,11 +1,11 @@
 (ns ujima.target
   (:require
-   [ujima.target.mock :as mock]
-   [ujima.target.rpi :as rpi]))
+   [ujima.target.mock :as mock-runtime]
+   [ujima.target.rpi.runtime :as rpi-runtime]))
 
 
 (defn ->runtime [env]
   (let [target (get-in env [:runtime :target])]
     (case target
-      :mock (mock/->runtime (get-in env [:runtime :mock]))
-      :rpi  (rpi/->runtime (get-in env [:runtime :rpi])))))
+      :mock (mock-runtime/->runtime (get-in env [:runtime :mock]))
+      :rpi  (rpi-runtime/->runtime (get-in env [:runtime :rpi])))))

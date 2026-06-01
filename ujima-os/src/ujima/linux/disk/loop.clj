da88844b5e1f03b68d3b770d2ace5ec7736118f6
vitalipe <vitali.pe@gmail.com>
2026-06-01 19:23:02 +0300

    loop checks and fixes

--- ujima-os/src/ujima/linux/disk/loop.clj
+++ ujima-os/src/ujima/linux/disk/loop.clj
@@ -1,17 +1,49 @@
 (ns ujima.linux.disk.loop
-  (:require [babashka.fs :as fs]
-            [ujima.linux.shell :refer [sudo$!]]))
+  (:require [clojure.string :as str]
+            [babashka.fs :as fs]
+            [cheshire.core :as json]
+
+            [ujima.linux.shell      :refer [sh sudo$!]]
+            [ujima.linux.disk       :refer [device->partitions]]
+            [ujima.linux.disk.mount :refer [device->mount-points]]))
 
 
-(defn attach-loopback-device! [image-path]
-  (when-not (fs/exists? image-path)
-    (throw (ex-info "Image file does not exist"
-                    {:image-path image-path})))
+(defn path->loopback-devices [image-path]
+  (let [{:keys [out]} (sh :losetup "-j" (str image-path))]
+    (->> (str/split-lines out)
+         (map #(first (str/split % #":" 2)))
+         (remove str/blank?)
+         (into []))))
 
-  (sudo$! losetup --find --show --partscan [image-path]))
+
+(defn loopback-devices []
+  (let [out (:out (sh :losetup "--list" "--json"))]
+    (-> out
+        (json/parse-string true)
+        (:loopdevices))))
+
+
+(defn attach-loopback-device! 
+  ([image-path readonly?] (attach-loopback-device! image-path false))
+  ([image-path readonly?]
+   (when-not (fs/exists? image-path)
+     (throw (ex-info "Image file does not exist"
+                     {:image-path image-path})))
+
+   (if readonly?
+     (sudo$! losetup --read-only --find --show --partscan [image-path])
+     (sudo$! losetup --find --show --partscan [image-path]))))
 
 
 (defn detach-loopback-device! [device]
+  (let [mnt-points (->> device device->partitions
+                     (mapcat device->mount-points))]
+    
+    (when-not (empty? mnt-points)
+      (throw
+        (ex-info "Device child partitions are mounted"
+                 {:device device :mount-points mnt-points}))))
+
   (sudo$! losetup --detach [device]))
 
 
